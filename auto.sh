fbc1d148396659814305a4fc185a497bebbb4a28
Lucas <EHSAN_SAGHAEE@YAHOO.COM>
2017-06-05 07:42:50 -0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 COUNTER=1
 while(true) do
-./Gard.sh
+./Gurd.sh
 curl "https://api.telegram.org/botتوکن/sendmessage" -F "chat_id=ایدی خودتون" -F "text=#NEWCRASH-#GardPCT-Reloaded-${COUNTER}-times"
 let COUNTER=COUNTER+1 
 done
